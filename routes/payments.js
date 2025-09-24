b82f7410539381e298776f7362cf13107a390389
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2025-09-25 00:31:28 +0530

    Update payments.js
    added the payment route

--- routes/payments.js
+++ routes/payments.js
@@ -8,7 +8,7 @@ const crypto = require("crypto")
 const router = express.Router()
 
 // Initialize Razorpay (you'll need to add credentials to .env)
-const razorpay = new Razorpay({
+const razorpay = new Razorpay({ 
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 })
