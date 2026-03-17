bf4f87bbdaa801d58c02e73fcce4d742abac73b2
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2026-03-17 13:16:13 +0530

    Update validators.js

--- utils/validators.js
+++ utils/validators.js
@@ -61,7 +61,31 @@ const validateContact = (data) => {
   return schema.validate(data)
 }
 
+const validateRegister = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(50).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string()
+      .pattern(/^[6-9]\d{9}$/)
+      .required(),
+    password: Joi.string().min(6).required(),
+  })
+
+  return schema.validate(data)
+}
+
+const validateLogin = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  })
+
+  return schema.validate(data)
+}
+
 module.exports = {
   validateBooking,
   validateContact,
+  validateRegister,
+  validateLogin,
 }
